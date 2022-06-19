61a05832fb49245de01adc03d71af0bc8bb24ef7
Misha-dev <shabatinmichael@mail.ru>
2022-06-19 19:23:44 +0500

    moved info about firebase into .env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID,
+} = process.env;
 const firebaseConfig = {
-  apiKey: "AIzaSyC5S7yfupNNl7dJUGZ51WLyXA4kKHmoMP4",
-  authDomain: "uploadimages-c763f.firebaseapp.com",
-  projectId: "uploadimages-c763f",
-  storageBucket: "uploadimages-c763f.appspot.com",
-  messagingSenderId: "982908399692",
-  appId: "1:982908399692:web:5a5c20df11d90428ab44e9",
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
